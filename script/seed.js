8592c720280ac371dd4c5116d9dd3e514caaf9ec
Celia Macrae <celiamacrae@gmail.com>
2020-02-03 12:01:23 -0500

    Added model and routes to send user data

--- script/seed.js
+++ script/seed.js
@@ -2,6 +2,7 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {UserData} = require('../server/db/models')
 
 async function seed() {
   await db.sync({force: true})
@@ -10,6 +11,77 @@ async function seed() {
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
+  ])
+
+  const datas = await Promise.all([
+    UserData.create({
+      rawData: [
+        {
+          NAME: 'Jonathan Sims',
+          DATE: '11/25/2029',
+          AGE: '37',
+          STATE: 'OH'
+        },
+        {NAME: 'Belle Cox', DATE: '01/29/1994', AGE: '28', STATE: 'IN'},
+        {
+          NAME: 'Calvin Estrada',
+          DATE: '06/26/1924',
+          AGE: '65',
+          STATE: 'SC'
+        },
+        {NAME: 'Roy Wright', DATE: '04/04/1996', AGE: '41', STATE: 'OH'},
+        {
+          NAME: 'Mayme Howell',
+          DATE: '08/22/1997',
+          AGE: '23',
+          STATE: 'AR'
+        },
+        {
+          NAME: 'Emma Morgan',
+          DATE: '12/24/2015',
+          AGE: '54',
+          STATE: 'AZ'
+        },
+        {
+          NAME: 'Jayden Anderson',
+          DATE: '03/12/2047',
+          AGE: '52',
+          STATE: 'AK'
+        },
+        {
+          NAME: 'Dennis Tyler',
+          DATE: '03/10/1978',
+          AGE: '44',
+          STATE: 'WA'
+        },
+        {
+          NAME: 'Jackson Strickland',
+          DATE: '05/28/1943',
+          AGE: '61',
+          STATE: 'UT'
+        },
+        {NAME: 'Henry Wise', DATE: '10/16/1919', AGE: '39', STATE: 'WA'}
+      ],
+      userId: 2
+    }),
+    UserData.create({
+      rawData: [
+        {
+          NAME: 'Dennis Tyler',
+          DATE: '03/10/1978',
+          AGE: '44',
+          STATE: 'WA'
+        },
+        {
+          NAME: 'Jackson Strickland',
+          DATE: '05/28/1943',
+          AGE: '61',
+          STATE: 'UT'
+        },
+        {NAME: 'Henry Wise', DATE: '10/16/1919', AGE: '39', STATE: 'WA'}
+      ],
+      userId: 1
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
