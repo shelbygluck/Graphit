668896d1b759e4129c40d54b912cb48904412d5f
anastasiia burlya <anastasiia.burlya@gmail.com>
2020-02-04 11:02:55 -0500

    feature: graph is showing the selected columns

--- client/components/line.js
+++ client/components/line.js
@@ -3,43 +3,45 @@ import {Line} from 'react-chartjs-2'
 import {connect} from 'react-redux'
 import {columnData} from '../store/data'
 
-let columns = ['STATE', 'AGE']
-let columnDataa = {
-  STATE: ['OH', 'IN', 'SC', 'OH', 'AR', 'AZ', 'AK', 'WA', 'UT', 'WA'],
-  AGE: ['37', '28', '65', '41', '23', '54', '52', '44', '61', '39']
-}
+// let columns = ['STATE', 'AGE']
+// let columnDataa = {
+//   STATE: ['OH', 'IN', 'SC', 'OH', 'AR', 'AZ', 'AK', 'WA', 'UT', 'WA'],
+//   AGE: ['37', '28', '65', '41', '23', '54', '52', '44', '61', '39']
+//}
 
 export class LineChart extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      labels: this.props.columnData[this.props.columns[0]],
-      datasets: [
-        {
-          label: 'Rainfall',
-          fill: false,
-          lineTension: 0.5,
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,0,1)',
-          borderWidth: 2,
-          data: this.props.columnData[this.props.columns[1]]
-        }
-      ]
+      labels: this.props.columnData[this.props.columns[1]],
+      data: this.props.columnData[this.props.columns[0]]
     }
   }
 
-  componentDidMount() {
-    this.props.loadColumnData(2)
+  async componentDidMount() {
+    await this.props.loadColumnData(2)
   }
 
   render() {
-    console.log(this.props.columnData[this.props.columns[0]], 'LABELS')
+    console.log(this.props.columnData[this.props.columns[1]], 'LABELS')
+    console.log(this.props.columnData[this.props.columns[0]], 'DATA')
+
     return (
       <div>
         <Line
           data={{
-            labels: this.state.labels,
-            datasets: this.state.datasets
+            labels: this.props.columnData[this.props.columns[1]],
+            datasets: [
+              {
+                label: 'Rainfall',
+                fill: false,
+                lineTension: 0.5,
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(0,0,0,1)',
+                borderWidth: 2,
+                data: this.props.columnData[this.props.columns[0]]
+              }
+            ]
           }}
           options={{
             title: {
