d19e7ebbd0f9dff6c85bcdee147711fec803237e
Celia Macrae <celiamacrae@gmail.com>
2020-02-06 17:21:00 -0500

    issue on last commit

--- client/helpers.js/tests/averageAndSum.spec.js
+++ client/helpers.js/tests/averageAndSum.spec.js
@@ -5,6 +5,7 @@ const {average, sum} = require('../averageAndSum')
 
 let labels = ['NY', 'NY', 'NY', 'CT', 'CT', 'CT', 'OH']
 let data = [1, 2, 3, 4, 5, 6, 7]
+let strData = ['1', '2', '3', '4', '5', '6', '7']
 
 // average(arr1, arr2)
 // sum(arr1, arr2)
@@ -12,10 +13,19 @@ describe('average', () => {
   it('averages numbers for data with repetitive labels', () => {
     expect(average(labels, data)).to.deep.equal([['NY', 'CT', 'OH'], [2, 5, 7]])
   })
+  it('parses string integers', () => {
+    expect(average(labels, strData)).to.deep.equal([
+      ['NY', 'CT', 'OH'],
+      [2, 5, 7]
+    ])
+  })
 })
 
 describe('sume', () => {
   it('sums numbers for data with repetitive labels', () => {
     expect(sum(labels, data)).to.deep.equal([['NY', 'CT', 'OH'], [6, 15, 7]])
+  })
+  it('parses string integers', () => {
+    expect(sum(labels, strData)).to.deep.equal([['NY', 'CT', 'OH'], [6, 15, 7]])
   })
 })
