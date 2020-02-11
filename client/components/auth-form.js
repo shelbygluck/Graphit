1e060097a46f224e22ead3e5e3dab5027751bf7d
Celia Macrae <celiamacrae@gmail.com>
2020-02-11 15:20:08 -0500

    updated signup and user to include first and last name

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+import {createUser} from '../store/user'
 
 /**
  * COMPONENT
@@ -29,7 +30,76 @@ const AuthForm = props => {
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      {/* <a href="/auth/google">{displayName} with Google</a> */}
+    </div>
+  )
+}
+const SignupForm = props => {
+  const {name, handleSubmit, error} = props
+
+  return (
+    <div id="main">
+      <div className="card">
+        <div className="modal-login modal-box modal-signUp">
+          <form onSubmit={handleSubmit} name={name}>
+            <div className="modal-body">
+              <div>
+                <label htmlFor="firstName">
+                  <small>First Name</small>
+                </label>
+                <input
+                  name="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="lastName">
+                  <small>Last Name</small>
+                </label>
+                <input
+                  name="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">
+                  <small>Email</small>
+                </label>
+                <input name="email" type="text" placeholder="Email" required />
+              </div>
+
+              <div>
+                <label htmlFor="password">
+                  <small>Password</small>
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+
+              <div>
+                <button type="submit" className="btn">
+                  Signup
+                </button>
+                {/* <a href="/auth/google" className="btn">
+                  Sign Up in with Google+
+                </a> */}
+              </div>
+            </div>
+
+            {error && error.response && <div> {error.response.data} </div>}
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
@@ -69,8 +139,24 @@ const mapDispatch = dispatch => {
   }
 }
 
+const mapDispatchSignUp = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const firstName = evt.target.firstName.value
+      const lastName = evt.target.lastName.value
+      const password = evt.target.password.value
+
+      const user = {firstName, lastName, email, password}
+      dispatch(createUser(user, formName))
+    }
+  }
+}
+
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatchSignUp)(SignupForm)
 
 /**
  * PROP TYPES
